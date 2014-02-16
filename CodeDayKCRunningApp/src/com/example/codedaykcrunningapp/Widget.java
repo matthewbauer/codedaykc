00b64688a8afe968611ac2410738e2960d5ea7bb
Matthew Bauer <mjbauer95@gmail.com>
2014-02-15 19:58:19 -0600

    Added media layout to widget.

--- CodeDayKCRunningApp/src/com/example/codedaykcrunningapp/Widget.java
+++ CodeDayKCRunningApp/src/com/example/codedaykcrunningapp/Widget.java
@@ -1,7 +1,13 @@
 package com.example.codedaykcrunningapp;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
 
 public class Widget extends AppWidgetProvider {
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
 
+	}
 }
